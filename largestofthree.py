f6f1259852e7ac5c686f610768a6ffcccb6508c8
Cassandra Geitner <109774822+NordicBlue5345@users.noreply.github.com>
2024-02-04 23:56:45 -0800

    Add files via upload

---
+++ largestofthree.py
@@ -0,0 +1,13 @@
+x = 5
+y = 10
+
+if x < 10:
+  if y < 5:
+    print("x is less than 10 and y is less than 5")
+  else:
+    print("x is less than 10 but y is greater than or equal to 5")
+else:
+  if y < 5:
+    print("x is greater than or equal to 10 and y is less than 5")
+  else:
+    print("x is greater than or equal to 10 and y is greater than or equal to 5")
